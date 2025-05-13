427084273efdd081ef4f9c7ac00ea169d69058fe
Joshua Tryon <joshuamtryon@gmail.com>
2025-05-12 23:03:56 -0500

    move-items solved

--- exercises/move-items.js
+++ exercises/move-items.js
@@ -2,7 +2,7 @@
  * SORTING NODES WITHIN A CONTAINER
  * Please, make sure to read the following files in the exercises-info folder before you start
  * * 01 SelectNodes.md
-*/
+ */
 
 /**
  * @task
@@ -12,8 +12,7 @@
  */
 
 // Your code goes here...
-
-
+const allItems = document.querySelectorAll(".item");
 
 /**
  * @task
@@ -23,8 +22,7 @@
  * */
 
 // Your code goes here
-
-
+const main = document.getElementById("main");
 
 /**
  * @task
@@ -34,8 +32,7 @@
  */
 
 // Your code goes here
-
-
+const favs = document.getElementById("favs");
 
 /**
  * @task
@@ -47,8 +44,21 @@
  */
 
 // Your code goes here
-
-
+function updateCollections(id, direction) {
+  const item = allItems[id - 1];
+  console.log("ID", id);
+  if (direction === "toFavs") {
+    main.removeChild(item);
+    item.children[0].classList.remove("fa-heart-circle-plus");
+    item.children[0].classList.add("fa-heart-crack");
+    favs.appendChild(item);
+  } else {
+    favs.removeChild(item);
+    item.children[0].classList.remove("fa-heart-crack");
+    item.children[0].classList.add("fa-heart-circle-plus");
+    main.appendChild(item);
+  }
+}
 
 /**
  * @task
@@ -65,5 +75,15 @@
  */
 
 // Your code goes here...
-
-
+allItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("here");
+    const parentId = item.parentElement.id;
+    const itemId = Number(item.id);
+    if (parentId === "main") {
+      updateCollections(itemId, "toFavs");
+    } else {
+      updateCollections(itemId, "toMain");
+    }
+  });
+});
